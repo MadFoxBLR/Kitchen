8556c4cd9260ba8749275982e2ad01b93ccaa9c2
MadFox <2991216@gmail.com>
2020-03-16 17:07:37 +0300

    Mask_phone_cheng

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,5 +1,9 @@
-$(document).ready(function(){
+
+/*$(document).ready(function () {
     $("#example1").inputmask("+375-99-999-99-99");
-    $("#example2").inputmask();
-});
-  
+    $("#example2").inputmask("+375-99-999-99-99");
+});*/
+
+$(document).ready(function () {
+$(".info_phone").mask("+375(99)999-99-99");
+})
